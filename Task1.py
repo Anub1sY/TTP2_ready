2044b0e93df241ddafbe67a61f6788b5516924e5
Anub1s_Y <cypyk138@mail.ru>
2023-12-24 18:46:00 +0700

    start_task1

--- Task1.py
+++ Task1.py
@@ -1,2 +1,18 @@
 import random
 from os import system
+
+
+def sum_sorted_arrays(array1, array2):
+    return 0
+
+
+def input_array(size):
+    return 0
+
+
+def generate_array(size):
+    return 0
+
+
+def start_task1():
+    pass
