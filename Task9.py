397e9826f9a1566c8b4a553a612a0682349b89a7
Anub1s_Y <cypyk138@mail.ru>
2023-12-24 19:04:43 +0700

    start_task9

--- Task9.py
+++ Task9.py
@@ -1,3 +1,26 @@
-from os import system
-from math import sqrt
 import random
+
+
+def check_distance(point1, point2):
+    pass
+
+
+def is_points_distance(array1, array2, distance):
+    pass
+
+
+def input_points(prompt):
+    pass
+
+
+def output_points(points):
+    pass
+
+
+def start_task9():
+    pass
+
+
+
+
+
