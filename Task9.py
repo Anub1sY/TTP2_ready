e5e79faac16a55b7b19612167fd3d3c22cd3cb36
Anub1s_Y <cypyk138@mail.ru>
2023-12-24 19:29:17 +0700

    add check_distance finction

--- Task9.py
+++ Task9.py
@@ -2,7 +2,15 @@ import random
 
 
 def check_distance(point1, point2):
-    pass
+    """
+    функция для вычисления расстояния между точками
+    :param point1: Первая точка
+    :param point2: Вторая точка
+    :return: Возвращает расстояние между двумя точками
+    """
+    x1, y1 = point1
+    x2, y2 = point2
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 
 def is_points_distance(array1, array2, distance):
