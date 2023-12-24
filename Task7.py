53a3cf9dadb37e46f768144b3c7f0ad501719c8b
Anub1s_Y <cypyk138@mail.ru>
2023-12-24 19:14:01 +0700

    add distance function

--- Task7.py
+++ Task7.py
@@ -4,7 +4,13 @@ from os import system
 
 
 def distance(point1, point2):
-    return 0
+    """
+    Функция рассчёта расстояния между точками
+    :param point1: Первая точка
+    :param point2: Вторая точка
+    :return: Расстояние между точками
+    """
+    return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
 
 def add_nearest_points(points):
