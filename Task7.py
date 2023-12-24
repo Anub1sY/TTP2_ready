090ab3b0fcc6e30a8585fee7b8f122ae285aa4de
Anub1s_Y <cypyk138@mail.ru>
2023-12-24 18:48:18 +0700

    start_task7

--- Task7.py
+++ Task7.py
@@ -1,3 +1,27 @@
 import random
 import math
 from os import system
+
+
+def distance(point1, point2):
+    return 0
+
+
+def add_nearest_points(points):
+    return 0
+
+
+def input_points():
+    return 0
+
+
+def generate_points(num_points):
+    return 0
+
+
+def output_points(points):
+    pass
+
+
+def start_task7():
+    pass
