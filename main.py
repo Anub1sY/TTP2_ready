e2d404ee188acac6abb219baf49f4b4118d8bcf3
Anub1s_Y <cypyk138@mail.ru>
2023-12-24 20:44:43 +0700

    first_commit

--- main.py
+++ main.py
@@ -2,7 +2,29 @@
 
 
 def menu():
-    pass
+    while True:
+        print('Главное меню:'
+              '1. Задание 1'
+              '2. Задание 7'
+              '3. Задание 8'
+              '4. Выход из программы ')
+
+        menu_point = input('Выберите пункт меню: ')
+
+        if menu_point == '1':
+            pass
+
+        elif menu_point == '2':
+            pass
+
+        elif menu_point == '3':
+            pass
+
+        elif menu_point == '4':
+            print('Выход из программы.')
+            break
+        else:
+            print('Некорректный выбор. Пожалуйста, выберите снова.')
 
 
 if __name__ == "__main__":
