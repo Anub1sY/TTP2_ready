5ffc0e3c3a95cb753204c0201ca9e100b2aa19b7
Anub1s_Y <cypyk138@mail.ru>
2023-12-24 19:35:00 +0700

    add points finction

--- Task9.py
+++ Task9.py
@@ -14,7 +14,18 @@ def check_distance(point1, point2):
 
 
 def is_points_distance(array1, array2, distance):
-    pass
+    """
+    Функция для находения точек, расстояние между которыми больше заданного числа
+    :param array1: Первая точка
+    :param array2: Вторая точка
+    :param distance: Расстояние между точками
+    :return: возвращает список точек
+    """
+    result_points = []
+    for point1, point2 in zip(array1, array2):
+        if check_distance(point1, point2) > distance:
+            result_points.append((point1, point2))
+    return result_points
 
 
 def input_points(prompt):
